f8c74db1ef8275bed6fa827fa0e6d4fa4704310d
Shivshakti-1 <105493291+Shivshakti-1@users.noreply.github.com>
2022-05-13 16:38:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,15 +24,90 @@ global.APIKeys = {
 global.owner = ['919172673114']
 global.premium = ['919172673114']
 global.ownernomer = '919172673114'
-global.ownername = 'RAJ💔'
-global.botname = 'RAJ BOT'
+global.ownername = 'SAY.SCOTCH💔'
+global.botname = '//═══════════════════════════════════════════════════════//
+//If you want to recode, reupload
+//or copy the codes/script,
+//pls give credit
+//no credit? i will take action immediately
+//© 2022 Aman Bot Inc. Pandu Bot 
+//Thank you to Lord Krishna, Family and Myself
+//════════════════════════════//
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+}
+
+//customize settings\\
+global.owner = ['919172673114']
+global.premium = ['919172673114']
+global.ownernomer = '919172673114'
+global.ownername = 'SAY.SCOTCH💔'
+global.botname = 'SAY.SCOTCH💔'
 global.footer = 'Aman Bot Inc.'
 global.ig = 'https://github.com/12345678-cyber'
-global.region = 'India, up west'
-global.sc = 'https://github.com/12345678-cyber/shivshakti0'
-global.myweb = 'not used'
-global.packname = 'RAJ Bot'
-global.author = 'RAJ'
+global.region = 'India, up east'
+global.sc = 'https://github.com'
+global.myweb = 'https://youtube.com'
+global.packname = 'Pandu Bot'
+global.author = '🚩 BHAGWADHARI 🚩'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: 'In Process...',
+    error: 'Error! Maybe Api Key Is Expired🤔!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./AmanMedia/pandupic.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
+'
+global.footer = 'Aman Bot Inc.'
+global.ig = 'https://github.com/12345678-cyber'
+global.region = 'India, up east'
+global.sc = 'https://github.com'
+global.myweb = 'https://youtube.com'
+global.packname = 'Pandu Bot'
+global.author = '🚩 BHAGWADHARI 🚩'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
